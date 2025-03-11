d041666d6624cc9a9cfbbcde5ffefbd5dccbcd04
Jessy QUAREZ <quarezjessy60@gmail.com>
2025-03-11 13:46:09 +0100

    Devoir Surveillé n°1 de SYNFONY terminée

--- src/Controller/MamieController.php
+++ src/Controller/MamieController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AjoutCafeType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Cafe;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class MamieController extends AbstractController
 {
@@ -12,6 +16,27 @@ final class MamieController extends AbstractController
     public function index(): Response
     {
         return $this->render('mamie/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/ajout_cafe', name: 'app_ajout_cafe')]
+    public function ajoutCafe(Request $request, EntityManagerInterface $em): Response
+    {
+        $cafe = new Cafe();
+        $form = $this->createForm(ajoutCafeType::class, $cafe);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($cafe);
+                $em->flush();
+                $this->addFlash('notice','Café ajouté avec succés');
+                return $this->redirectToRoute('app_ajout_cafe');
+            }
+        }
+
+        return $this->render('mamie/ajout_cafe.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
